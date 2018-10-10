3411d688895b166f4293a884ce409cce0ddc9104
Robert-Thinkpad <rob5300@hotmail.com>
2018-10-10 13:10:13 +0100

    City and Resource outlining.

--- Assets/0 Scripts/Infustructure/City.cs
+++ Assets/0 Scripts/Infustructure/City.cs
@@ -1,9 +1,27 @@
-﻿using System.Collections.Generic;
+﻿using Resources;
+using System.Collections.Generic;
 
 namespace Infustructure {
     public class City {
 
+        public string Name;
+        public string Owner;
+        
+        protected Dictionary<System.Type, Resource> Resources;
 
-	
+        public City(string name, string owner)
+        {
+            name = name != string.Empty ? name : System.DateTime.Now.ToShortTimeString() + ":" + System.DateTime.Now.ToShortDateString();
+            owner = owner != string.Empty ? owner : "Mayor";
+
+            Resources = new Dictionary<System.Type, Resource>();
+        }
+
+        public Resource GetResource<T>() where T : Resource {
+            if (Resources.ContainsKey(typeof(T))) return Resources[typeof(T)];
+            else return null;
+        }
+
+
     }
 }
---
+++ Assets/0 Scripts/Resource/Resource.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Resources
+{
+    public class Resource
+    {
+        public Resource()
+        {
+
+        }
+
+        public int Avaliable { get; private set; }
+
+        public void Add(int amount)
+        {
+            Avaliable += amount;
+        }
+    }
+}
