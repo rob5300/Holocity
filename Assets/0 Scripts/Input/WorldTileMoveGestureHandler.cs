6126d63d29ad67fd3a61aca403af115a7c25ee67
DamianBogus <damian.boguslawski@hotmail.co.uk>
2018-11-11 18:13:43 +0000

    Added voice commands. Recognition is good. Move, Rotate and Swap is working well.

--- Assets/0 Scripts/Input/WorldTileMoveGestureHandler.cs
+++ Assets/0 Scripts/Input/WorldTileMoveGestureHandler.cs
@@ -6,68 +6,72 @@ using HoloToolkit.Unity.InputModule;
 
 public class WorldTileMoveGestureHandler : MonoBehaviour, IManipulationHandler {
 
-    public float RotationSpeed = 5;
+    
     //not sure about using a vector as it has to be reset after every manipulation and would be annoying to change if we ever wanted to.
     public Vector3 MoveOffset = new Vector3(0, 0.15f, 0);
 
     private WorldGridTile _tileParent;
     private Vector3 _startPosition;
+    private VoiceGestureControl _voiceCommand;
 
     public void Start()
     {
         _tileParent = GetComponentInParent<WorldGridTile>();
+        _voiceCommand = InputManager.Instance.transform.GetComponent<VoiceGestureControl>();
     }
-
-    //When the move gesture begins
+    
     void IManipulationHandler.OnManipulationStarted(ManipulationEventData eventData)
     {
+        if (_voiceCommand.IsNavigating) return;
+
         InputManager.Instance.PushModalInputHandler(gameObject);
 
         _startPosition = transform.localPosition;
-        //need to also set the MoveOffset to the new position
-        //transform.position += MoveOffset;
         MoveOffset = transform.position + MoveOffset;
 
         gameObject.layer = LayerMask.NameToLayer("Ignore Raycast");
+        eventData.Use();
     }
 
     void IManipulationHandler.OnManipulationUpdated(ManipulationEventData eventData)
     {
-        if (!InputManager.Instance.CheckModalInputStack(gameObject)) return;
+        if (!InputManager.Instance.CheckModalInputStack(gameObject) || _voiceCommand.IsNavigating) return;
 
         Vector3 gestureMovemnt = eventData.CumulativeDelta;
         gestureMovemnt.y = 0;
-
-        //This line results in accelerated movement hard to control, The MoveOffset acts as an origin point
-        //The reason why you can't move the house properly with my version is controller related. 
-        //the joystick only gives you x and y... by all means it should work properly in the actual HoloLens 
-        //transform.position += gestureMovemnt;
-
         transform.position = MoveOffset + gestureMovemnt;
+
+        eventData.Use();
 
     }
 
     void IManipulationHandler.OnManipulationCompleted(ManipulationEventData eventData)
     {
-        InputManager.Instance.PopModalInputHandler();
-        //Not sure about these two lines, why are you subtracting and then adding?
-        //transform.position -= MoveOffset;
-        //_tileParent.AttemptBuildingSwap(transform.position + MoveOffset);
-        _tileParent.AttemptBuildingSwap(transform.position);
+        if (_voiceCommand.IsNavigating) return;
 
-        transform.localPosition = _startPosition;
-        gameObject.layer = LayerMask.NameToLayer("Hologram");
+         InputManager.Instance.PopModalInputHandler();
+         _tileParent.AttemptBuildingSwap(transform.position);
+
+         transform.localPosition = _startPosition;
+         gameObject.layer = LayerMask.NameToLayer("Hologram");
 
         //Using this line to reset the MoveOffset for now
-        MoveOffset = new Vector3(0, 0.15f, 0);
+         MoveOffset = new Vector3(0, 0.15f, 0);
+
+        eventData.Use();
     }
 
     void IManipulationHandler.OnManipulationCanceled(ManipulationEventData eventData)
     {
+        if (_voiceCommand.IsNavigating) return;
+
         InputManager.Instance.PopModalInputHandler();
         transform.localPosition = _startPosition;
+
         //Using this line to reset the MoveOffset for now
         MoveOffset = new Vector3(0, 0.15f, 0);
+
+        eventData.Use();
     }
 
 }
