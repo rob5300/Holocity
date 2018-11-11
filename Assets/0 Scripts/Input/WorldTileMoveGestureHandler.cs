bd0f39203b4951b3e34d88ad9c9482b44b031e25
DamianBogus <damian.boguslawski@hotmail.co.uk>
2018-11-11 14:57:14 +0000

    Changed up the WorldTileMoveGestureHandler script, added comments wherever something was changed.

--- Assets/0 Scripts/Input/WorldTileMoveGestureHandler.cs
+++ Assets/0 Scripts/Input/WorldTileMoveGestureHandler.cs
@@ -7,6 +7,7 @@ using HoloToolkit.Unity.InputModule;
 public class WorldTileMoveGestureHandler : MonoBehaviour, IManipulationHandler {
 
     public float RotationSpeed = 5;
+    //not sure about using a vector as it has to be reset after every manipulation and would be annoying to change if we ever wanted to.
     public Vector3 MoveOffset = new Vector3(0, 0.15f, 0);
 
     private WorldGridTile _tileParent;
@@ -23,7 +24,9 @@ public class WorldTileMoveGestureHandler : MonoBehaviour, IManipulationHandler {
         InputManager.Instance.PushModalInputHandler(gameObject);
 
         _startPosition = transform.localPosition;
-        transform.position += MoveOffset;
+        //need to also set the MoveOffset to the new position
+        //transform.position += MoveOffset;
+        MoveOffset = transform.position + MoveOffset;
 
         gameObject.layer = LayerMask.NameToLayer("Ignore Raycast");
     }
@@ -34,22 +37,37 @@ public class WorldTileMoveGestureHandler : MonoBehaviour, IManipulationHandler {
 
         Vector3 gestureMovemnt = eventData.CumulativeDelta;
         gestureMovemnt.y = 0;
-        transform.position += gestureMovemnt;
+
+        //This line results in accelerated movement hard to control, The MoveOffset acts as an origin point
+        //The reason why you can't move the house properly with my version is controller related. 
+        //the joystick only gives you x and y... by all means it should work properly in the actual HoloLens 
+        //transform.position += gestureMovemnt;
+
+        transform.position = MoveOffset + gestureMovemnt;
+
     }
 
     void IManipulationHandler.OnManipulationCompleted(ManipulationEventData eventData)
     {
         InputManager.Instance.PopModalInputHandler();
-        transform.position -= MoveOffset;
-        _tileParent.AttemptBuildingSwap(transform.position + MoveOffset);
+        //Not sure about these two lines, why are you subtracting and then adding?
+        //transform.position -= MoveOffset;
+        //_tileParent.AttemptBuildingSwap(transform.position + MoveOffset);
+        _tileParent.AttemptBuildingSwap(transform.position);
+
         transform.localPosition = _startPosition;
         gameObject.layer = LayerMask.NameToLayer("Hologram");
+
+        //Using this line to reset the MoveOffset for now
+        MoveOffset = new Vector3(0, 0.15f, 0);
     }
 
     void IManipulationHandler.OnManipulationCanceled(ManipulationEventData eventData)
     {
         InputManager.Instance.PopModalInputHandler();
         transform.localPosition = _startPosition;
+        //Using this line to reset the MoveOffset for now
+        MoveOffset = new Vector3(0, 0.15f, 0);
     }
 
 }
