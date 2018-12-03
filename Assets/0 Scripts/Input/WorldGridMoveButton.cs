b634a3f40b3b04f9d834c988e6bc8ba93aa0407a
Damian <damian.boguslawski@hotmail.co.uk>
2018-12-03 11:26:26 +0000

    Fixed grid movement script.

--- Assets/0 Scripts/Input/WorldGridMoveButton.cs
+++ Assets/0 Scripts/Input/WorldGridMoveButton.cs
@@ -9,23 +9,62 @@ public class WorldGridMoveButton : MonoBehaviour ,IManipulationHandler
 
     private Vector3 _startPosition;
     private Vector3 _gridStartPosition;
-
+/*
     public void OnManipulationCanceled(ManipulationEventData eventData)
     {
-        transform.SetParent(GridParent.GridContainer.transform);
+        //transform.SetParent(GridParent.GridContainer.transform);
+        //InputManager.Instance.PushModalInputHandler(gameObject);
+
     }
 
     public void OnManipulationCompleted(ManipulationEventData eventData)
     {
+        //transform.SetParent(GridParent.GridContainer.transform);
+    }
+
+    public void OnManipulationStarted(ManipulationEventData eventData)
+    {
+        Debug.Log("Move button Start");
+        //transform.SetParent(null);
+        _startPosition = transform.parent.position;
+    }
+
+    public void OnManipulationUpdated(ManipulationEventData eventData)
+    {
+        Debug.Log("Move button Update");
+        transform.position = _startPosition + eventData.CumulativeDelta;
+    }
+*/
+
+
+    public void OnManipulationCanceled(ManipulationEventData eventData)
+    {
+        InputManager.Instance.PopModalInputHandler();
         transform.SetParent(GridParent.GridContainer.transform);
+
+        eventData.Use();
+
+    }
+
+    public void OnManipulationCompleted(ManipulationEventData eventData)
+    {
+        InputManager.Instance.PopModalInputHandler();
+        transform.SetParent(GridParent.GridContainer.transform);
+        eventData.Use();
+
     }
 
     public void OnManipulationStarted(ManipulationEventData eventData)
     {
 		Debug.Log("Move button Start");
         transform.SetParent(null);
+
+        InputManager.Instance.PushModalInputHandler(gameObject);
+
         _startPosition = transform.position;
         _gridStartPosition = GridParent.GridContainer.transform.position;
+        eventData.Use();
+
     }
 
     public void OnManipulationUpdated(ManipulationEventData eventData)
@@ -33,5 +72,8 @@ public class WorldGridMoveButton : MonoBehaviour ,IManipulationHandler
 		Debug.Log("Move button Update");
         transform.position = _startPosition + eventData.CumulativeDelta;
         GridParent.GridContainer.transform.position = _gridStartPosition + eventData.CumulativeDelta;
+        eventData.Use();
+
     }
+
 }
