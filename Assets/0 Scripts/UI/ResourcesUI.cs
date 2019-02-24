37dde3e23b3a625124946b26150077e1e39c7436
Robert-Thinkpad <rob5300@hotmail.com>
2019-02-24 14:11:37 +0000

    Fixed ResourcesUI and did a manual merge of Damian's changes. Added stupid meta file.

--- Assets/0 Scripts/UI/ResourcesUI.cs
+++ Assets/0 Scripts/UI/ResourcesUI.cs
@@ -1,5 +1,6 @@
 ﻿using CityResources;
 using System;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
@@ -14,38 +15,65 @@ public class ResourcesUI : MonoBehaviour
     public Toggle rotate;
     public Color ToggleColor;
 
-    private Electricity _electricity;
-    private Water _water;
     private float _lastUpdateTime = -999;
     [NonSerialized]
     public WorldGrid WorldGrid;
 
-    //Rotate resource towards player
-    private Camera camera;
+    private Camera lookCamera;
+    private List<ResourceData> waterResources;
+    private List<ResourceData> electricityResources;
 
     public void Start()
     {
-        UIManager.Instance.StateChanged += ToggleUI;
-        _electricity = Game.CurrentSession.City.GetResource<Electricity>();
-        _water = Game.CurrentSession.City.GetResource<Water>();
-        camera = GameObject.Find("MixedRealityCamera").GetComponent<Camera>();
+		UIManager.Instance.StateChanged += ToggleUI;
+        lookCamera = GameObject.Find("MixedRealityCamera").GetComponent<Camera>();
         SwitchMode(GesturesManager.Instance.IsNavigating);
     }
 
     public void Update()
     {
-        transform.rotation = Quaternion.LookRotation(camera.transform.forward);
+        //Look at the user each frame
+        transform.rotation = Quaternion.LookRotation(lookCamera.transform.forward);
+
         //Update the resources ui for city resources.
         if(_lastUpdateTime + UpdateDelay < Time.time)
         {
-            //Electricity
-            ResourceDataObjects[0].ValueText.text = _electricity.Value.ToString();
-            ResourceDataObjects[0].InputText.text = _electricity.AddedLastTick.ToString();
-            ResourceDataObjects[0].OutputText.text = _electricity.RecievedLastTick.ToString();
-            //Water
-            ResourceDataObjects[1].ValueText.text = _water.Value.ToString();
-            ResourceDataObjects[1].InputText.text = _water.AddedLastTick.ToString();
-            ResourceDataObjects[1].OutputText.text = _water.RecievedLastTick.ToString();
+            CheckForLists();
+
+            if (electricityResources != null)
+            {
+                //Electricity
+                float elecVal = 0;
+                float elecInput = 0;
+                float elecOut = 0;
+                foreach (ResourceData r in electricityResources)
+                {
+                    elecVal += r.resource.Value;
+                    elecInput += r.resource.AddedLastTick;
+                    elecOut += r.resource.RecievedLastTick;
+                }
+                ResourceDataObjects[0].ValueText.text = elecVal.ToString();
+                ResourceDataObjects[0].InputText.text = elecInput.ToString();
+                ResourceDataObjects[0].OutputText.text = elecOut.ToString(); 
+            }
+
+            if (waterResources != null)
+            {
+                //Water
+                float waterVal = 0;
+                float waterInput = 0;
+                float waterOut = 0;
+                foreach (ResourceData r in waterResources)
+                {
+                    waterVal += r.resource.Value;
+                    waterInput += r.resource.AddedLastTick;
+                    waterOut += r.resource.RecievedLastTick;
+                }
+                ResourceDataObjects[1].ValueText.text = waterVal.ToString();
+                ResourceDataObjects[1].InputText.text = waterInput.ToString();
+                ResourceDataObjects[1].OutputText.text = waterOut.ToString();
+            }
+
             _lastUpdateTime = Time.time;
             //Update Money
             FundsText.text = Game.CurrentSession.Funds.ToString();
@@ -60,7 +88,7 @@ public class ResourcesUI : MonoBehaviour
     {
         if (navigate)
         {
-          
+
             rotate.image.color = ToggleColor;
             move.image.color = Color.white;
         }
@@ -71,10 +99,22 @@ public class ResourcesUI : MonoBehaviour
         }
         GesturesManager.Instance.SwitchMode(navigate);
     }
-
-    void ToggleUI(bool check)
+	
+	void ToggleUI(bool check)
     {
         gameObject.SetActive(check);
+    }
+
+    private void CheckForLists()
+    {
+        if (waterResources == null)
+        {
+            waterResources = WorldGrid.GridSystem.GetResourceList(typeof(Water));
+        }
+        if (electricityResources == null)
+        {
+            electricityResources = WorldGrid.GridSystem.GetResourceList(typeof(Electricity));
+        }
     }
 }
 
